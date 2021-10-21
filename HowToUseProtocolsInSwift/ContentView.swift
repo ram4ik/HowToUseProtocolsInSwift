c1b847ecc03cca490917e650b91b1c68a9e44fd7
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-10-21 11:17:02 +0300

    init

--- HowToUseProtocolsInSwift/ContentView.swift
+++ HowToUseProtocolsInSwift/ContentView.swift
@@ -7,15 +7,48 @@
 
 import SwiftUI
 
+protocol ColorThemeProtocol {
+    var primary: Color { get }
+    var secondary: Color { get }
+    var tertiary: Color { get }
+}
+
+struct DefaultColorTheme: ColorThemeProtocol {
+    let primary: Color = .blue
+    let secondary: Color = .white
+    let tertiary: Color = .gray
+}
+
+struct AlternativeColorTheme: ColorThemeProtocol {
+    let primary: Color = .red
+    let secondary: Color = .white
+    let tertiary: Color = .green
+}
+
 struct ContentView: View {
+    
+    let colorTheme: ColorThemeProtocol
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            colorTheme.tertiary
+                .ignoresSafeArea()
+            
+            Text("Protocols are awesome!")
+                .font(.headline)
+                .foregroundColor(colorTheme.secondary)
+                .padding()
+                .background(colorTheme.primary)
+                .cornerRadius(10)
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView(colorTheme: DefaultColorTheme())
+            ContentView(colorTheme: AlternativeColorTheme())
+        }
     }
 }
--- HowToUseProtocolsInSwift/HowToUseProtocolsInSwiftApp.swift
+++ HowToUseProtocolsInSwift/HowToUseProtocolsInSwiftApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct HowToUseProtocolsInSwiftApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(colorTheme: DefaultColorTheme())
         }
     }
 }
