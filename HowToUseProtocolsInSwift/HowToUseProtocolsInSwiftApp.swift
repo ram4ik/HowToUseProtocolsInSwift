8a0dc0a526853ca46afc52c4aa0663e0141f077e
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-10-21 11:03:28 +0300

    Initial Commit

---
+++ HowToUseProtocolsInSwift/HowToUseProtocolsInSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowToUseProtocolsInSwiftApp.swift
+//  HowToUseProtocolsInSwift
+//
+//  Created by ramil on 21.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct HowToUseProtocolsInSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
